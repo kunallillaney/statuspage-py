f6c4edd2f5ddf8a32127eb617d9eabb19ea2356f
Kunal Lillaney <lillaney@jhu.edu>
2017-01-21 01:32:17 +0530

    [test] Adding test

--- test/test_component.py
+++ test/test_component.py
@@ -1,11 +1,43 @@
+# Copyright 2016 Kunal Lillaney (http://kunallillaney.github.io)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import pytest
 import sys
 sys.path.append('..')
 from core.component import Component
 
-c = Component()
-c.name = 'test'
-c.desciption = 'Testing'
-import pdb; pdb.set_trace()
-comp = Component.fromName('test')
-# c.create()
+class Test_Component:
+
+  def setup_class(self):
+    self.component = Component()
+    self.component.name = 'Test_Name_1'
+    self.component.description = 'Test_Description_1'
+
+  def teardown_class(self):
+    pass
+
+  def test_create_component(self):
+    self.component.create()
+    
+  def test_get_component(self):
+    import pdb; pdb.set_trace()
+    component = Component.fromName('Test_Name_1')
+    assert(component.name == self.component.name)
+    assert(component.id == self.component.id)
+    assert(component.description == self.component.description)
+
+  def test_delete_component(self):
+    import pdb; pdb.set_trace()
+    self.component.delete()
+    component = Component.fromName('Test_Name_1')
